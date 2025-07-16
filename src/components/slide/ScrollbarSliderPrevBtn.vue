8a554cc4f3d0407280bd683da7e61a20613605f8
Kayque Goldner <kayquegoldner@gmail.com>
2025-07-16 16:34:25 -0300

    add home page card list

---
+++ src/components/slide/ScrollbarSliderPrevBtn.vue
@@ -0,0 +1,12 @@
+<template>
+  <button
+    class="w-max h-max p-2 bg-slate-950 hover:bg-destructive disabled:opacity-50 disabled:pointer-events-none rounded-[6px] transition ease-out duration-150"
+    v-bind="$attrs"
+  >
+    <ArrowLeft class="h-4 w-4 text-white" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ArrowLeft } from "lucide-vue-next";
+</script>
